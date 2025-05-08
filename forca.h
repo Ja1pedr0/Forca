317ae711cde9a33bfd4a623ef69d2a21125105cb
Joao Pedro <joaopedroevangelista63@gmail.com>
2025-05-08 17:32:04 -0300

    anotacoes e funcoes

---
+++ forca.h
@@ -0,0 +1,4 @@
+void carregarPalavras()
+void desenharForca()
+verificarLetra()
+registrarResultado()
---
+++ main.c
@@ -0,0 +1,26 @@
+#include "forca.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	//Definir qual será a palavra através do palavras.txt
+	
+	//Deve selecionar de maneira aleatória.
+	//Preciso definir de um jeito que eu consiga separar as letras.
+	//Rodar o jogo da forca:
+	//Exibir o desenho da forca
+	//exibir a quantidade de _ de acordo com o número de letras
+	//pedir ao jogador para entrar com uma letra
+	//se a letra não foi usada:
+	//verificar se existe a letra na palavra.
+	//se existe: deve substituir o _ pela letra
+	//se não existe: adicionar uma parte do boneco na forca.
+	//se a letra foi usada: avisar na tela e pedir para inserir outra.
+	//escrever derrota ou vitoria dependendo da condição
+	//exibir a palavra
+	//guardar o nome do jogador
+	//escrever em resultados.txt o nome, a palavra sorteada, e se perdeu ou ganhou.
+	//perguntar se deseja jogar novamente
+
+}
